46b1ecdecfc921b11eefdaade78ea966707affdb
renan <renancunhafonseca@gmail.com>
2020-05-24 17:10:58 -0300

    Refactor neural_network.py, change variable names

--- src/feed_forward/neural_network.py
+++ src/feed_forward/neural_network.py
@@ -1,10 +1,6 @@
 import numpy as np
-from typing import List
-from sklearn.metrics import mean_squared_error
-from abc import ABC, abstractmethod
 from typing import Tuple, List
-from src.feed_forward.layers import LinearLayer, SigmoidLayer, CrossEntropyLoss, ParamLayer, Layer, BatchNormLayer
-import pandas as pd
+from src.feed_forward.layers import CrossEntropyLoss, ParamLayer, Layer, BatchNormLayer
 from tqdm import tqdm
 import math
 
@@ -38,40 +34,31 @@ class NeuralNetwork:
         accuracy = np.sum(y_labels == y_pred_labels) / test_x.shape[0]
         return float(np.mean(loss)), accuracy
 
-    def fit(self, x: np.ndarray, y: np.ndarray,
+    def fit(self, x_train: np.ndarray, y_train: np.ndarray,
             learning_rate: float, steps: int,
-            batch_size: int, test_x: np.ndarray, 
-            test_y: np.ndarray) -> np.array:
+            batch_size: int, x_test: np.ndarray,
+            y_test: np.ndarray) -> np.array:
         
-        num_examples = x.shape[0]
+        num_examples = x_train.shape[0]
         num_batches = math.ceil(num_examples/batch_size)
-        metrics = np.zeros((steps//num_batches, 2))
+        metrics = np.zeros((steps//num_batches, 2))  # loss, accuracy
         random_index = np.linspace(0, num_examples-1, num_examples).astype(int)
-        step = 0
-        pbar = tqdm(total=steps)
-        metrics_index = 0
-        while step < steps:
+        for epoch in tqdm(range(steps//num_batches)):
             np.random.shuffle(random_index)
-            x = x[random_index]
-            y = y[random_index]
+            x_train = x_train[random_index]
+            y_train = y_train[random_index]
             for index_batch in range(0, num_examples, batch_size):
-                mini_batch_x = x[index_batch: index_batch + batch_size]
-                mini_batch_y = y[index_batch: index_batch + batch_size]
+                mini_batch_x = x_train[index_batch: index_batch + batch_size]
+                mini_batch_y = y_train[index_batch: index_batch + batch_size]
                 self.loss_layer = CrossEntropyLoss(mini_batch_y)
                 y_pred = self.predict(mini_batch_x, test=False)
                 self.loss_layer.forward(y_pred)
                 self.backward_propagation(y_pred)
                 self.update_params(learning_rate)
 
-                step += 1
-                pbar.update(1)
-                if step > steps:
-                    break
-            
-            loss, accuracy = self.evaluate_model(test_x, test_y, x)
-            metrics[metrics_index, :] = loss, accuracy
-            metrics_index += 1
-            print(f"Step {step}")
+            loss, accuracy = self.evaluate_model(x_test, y_test, x_train)
+            metrics[epoch, :] = loss, accuracy
+            print(f"Epoch {epoch}")
             print(f"Loss: {loss} | Accuracy: {accuracy}")                   
 
         return metrics
